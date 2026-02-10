67d6512d1e196f36aefe27680183eb8f3ee86db2
AbhayKumar2107 <abhaykumarpatelrbl@gmail.com>
2026-02-10 20:47:31 +0530

    added firebase projects

---
+++ Firebase/q2_starter/q2_starter/src/App.js
@@ -0,0 +1,117 @@
+import { useState, useReducer } from "react";
+import { doc, collection, addDoc, setDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
+import { db } from "./Firebaseinit";
+import "./App.css";
+
+// components imports
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+
+// react toasts
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import firebase methods here
+
+const reducer = (state, action) => {
+  const { payload } = action;
+  switch (action.type) {
+    case "ADD_EXPENSE": {
+      return {
+        expenses: [payload.expense, ...state.expenses]
+      };
+    }
+    case "REMOVE_EXPENSE": {
+      return {
+        expenses: state.expenses.filter((expense) => expense.id !== payload.id)
+      };
+    }
+    case "UPDATE_EXPENSE": {
+      const expensesDuplicate = state.expenses;
+      expensesDuplicate[payload.expensePos] = payload.expense;
+      return {
+        expenses: expensesDuplicate
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { expenses: [] });
+  const [expenseToUpdate, setExpenseToUpdate] = useState(null);
+
+  const addExpense = async (expense) => {
+    const expenseRef = collection(db, "expenses");
+    const docRef = await addDoc(expenseRef, expense);
+
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: { expense: { id: docRef.id, ...expense } }
+    });
+    toast.success("Expense added successfully.");
+  };
+
+  const deleteExpense = (id) => {
+    dispatch({ type: "REMOVE_EXPENSE", payload: { id } });
+  };
+
+  const resetExpenseToUpdate = () => {
+    setExpenseToUpdate(null);
+  };
+
+  const updateExpense = async (expense) => {
+    const expensePos = state.expenses
+      .map((exp) => exp.id)
+      .indexOf(expense.id);
+
+    if (expensePos === -1) return false;
+
+    try {
+      const expenseRef = doc(db, "expenses", expense.id);
+
+      await setDoc(expenseRef, {
+        ...expense,
+        updatedAt: new Date()
+      });
+
+      dispatch({
+        type: "UPDATE_EXPENSE",
+        payload: { expensePos, expense }
+      });
+
+      toast.success("Expense updated successfully.");
+    } catch (error) {
+      toast.error("Failed to update expense");
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer />
+      <h2 className="mainHeading">Expense Tracker</h2>
+      <div className="App">
+        <ExpenseForm
+          addExpense={addExpense}
+          expenseToUpdate={expenseToUpdate}
+          updateExpense={updateExpense}
+          resetExpenseToUpdate={resetExpenseToUpdate}
+        />
+        <div className="expenseContainer">
+          <ExpenseInfo expenses={state.expenses} />
+          <ExpenseList
+            expenses={state.expenses}
+            deleteExpense={deleteExpense}
+            changeExpenseToUpdate={setExpenseToUpdate}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Firebase/q2_starter/q2_starter/src/Firebaseinit.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_JuNgAbimbbDyYHv_SI70cUDh0E4qIgQ",
+  authDomain: "blogging-app-836f7.firebaseapp.com",
+  projectId: "blogging-app-836f7",
+  storageBucket: "blogging-app-836f7.firebasestorage.app",
+  messagingSenderId: "283455765212",
+  appId: "1:283455765212:web:c936286b1ac3fa11299219"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
