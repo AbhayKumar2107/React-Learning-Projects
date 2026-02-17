3aae90d2b48f471fec76729173def8ec9d87b66c
AbhayKumar2107 <abhaykumarpatelrbl@gmail.com>
2026-02-17 18:04:38 +0530

    Added react router practice projects

---
+++ ReactRouter/q4_starter/q4_starter/src/App.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { List } from "./pages/List";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./components/Navbar";
+
+export default function App() {
+  // refactor the code below
+   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/home", element: <Home /> },
+        { path: "/list", element: <List /> },
+        { path: "/contact", element: <Contact /> }
+      ]
+    }
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
---
+++ ReactRouter/q7_starter/q7_starter/src/pages/NotFound.js
@@ -0,0 +1,23 @@
+// import hook here
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+
+export const NotFound = () => {
+  // create back funtion here
+  const navigate = useNavigate();
+
+  useEffect(() => {
+  setTimeout(() => navigate(), 3000);
+}, []);
+  return (
+    <div className="not-found page">
+      <h3>Page not found.</h3>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/2748/2748558.png"
+        alt="not found"
+      />
+
+      <button  onClick={() => navigate("/")}>Back to Home</button>
+    </div>
+  );
+};
